92bbcbc47f54d0a22226b3f7854f1622ac097b00
Elizabeth L <onceady4@gmail.com>
2023-12-01 18:04:59 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,345 @@
+//the tags aren't used in this program
+var items = {
+  series: [
+    {
+      name:"5 Worlds",
+      author:"Mark Siegel, Alexis Siegel, Xanthe Bouma, Matt Rockefeller, Boya Sun",
+      titles:"The Sand Warrior, The Cobalt Prince, The Red Maze, The Amber Anthem, The Emerald Gate",
+      tgs:"graphic fantasy kids"
+    },
+    {
+      name:"Arlo Finch",
+      author:"John August",
+      titles:"Arlo Finch in the Valley of Fire, Arlo Finch in the Lake of the Moon, Arlo Finch in the Kingdom of Shadows",
+      tgs:"fantasy kids paranormal"
+    },
+    {
+      name:"Artemis Fowl",
+      author:"Eoin Colfer",
+      titles:"Artemis Fowl, The Arctic Incident, The Eternity Code, The Opal Deception, The Lost Colony, The Time Paradox, The Atlantis Complex, The Last Guardian",
+      tgs:"classic fantasy sci-fi"
+    },
+    {
+      name:"A Series of Unfortunate Events",
+      author:"Lemony Snicket",
+      titles:"The Bad Beginning, The Reptile Room, The Wide Window, The Miserable Mill, The Austere Academy, The Ersatz Elevator, The Vile Village, The Hostile Hospital, The Carnivorous Carnival, The Slippery Slope, The Grim Grotto, The Penultimate Peril, The End",
+      tgs:"classic wordplay"
+    },
+    {
+      name:"A Tale Dark and Grimm",
+      author:"Adam Gidwitz",
+      titles:"A Tale Dark and Grimm, In a Glass Grimmly, The Grimm Conclusion",
+      tgs:"fantasy wordplay"
+    },
+    {
+      name:"Carve the Mark",
+      author:"Veronica Roth",
+      titles:"Carve the Mark, The Fates Divide",
+      tgs:"fantasy sci-fi ya"
+    },
+    {
+      name:"Escape From Mr. Lemoncello's Library",
+      author:"Chris Grabenstein",
+      titles:"Escape From Mr. Lemoncello's Library, Mr. Lemoncello's Library Olympics, Mr. Lemoncello's Great Library Race, Mr. Lemoncello's All-Star Breakout Game, Mr. Lemoncello and the Titanium Ticket",
+      tgs:"kids realistic wordplay"
+    },
+    {
+      name:"Goosebumps",
+      author:"R. L. Stine",
+      titles:"too many",
+      tgs:"mystery kids paranormal"
+    },
+    {
+      name:"Hildafolk",
+      author:"Luke Pearson",
+      titles:"Hildafolk/Hilda and the Troll, Hilda and the Midnight Giant, Hilda and the Bird Parade, Hilda and the Black Hound, Hilda and the Stone Forest, Hilda and the Mountain King",
+      tgs:"graphic fantasy kids"
+    },
+    {
+      name:"Magic Tree House",
+      author:"Mary Pope Osborne",
+      titles:"too many",
+      tgs:"kids fantasy historical informational"
+    },
+    {
+      name:"Prisoner of Ice and Snow",
+      author:"Ruth Lauren",
+      titles:"Prisoner of Ice and Snow, Seeker of the Crown",
+      tgs:"fantasy historical"
+    },
+    {
+      name:"Septimus Heap",
+      author:"Angie Sage",
+      titles:"Magyk, Flyte, Physik, Queste, Syren, Darke, Fyre",
+      tgs:"fantasy historical"
+    },
+    {
+      name:"Shadow and Bone",
+      author:"Leigh Bardugo",
+      titles:"Shadow and Bone, Siege and Storm, Ruin and Rising",
+      tgs:"fantasy romance ya"
+    },
+    {
+      name:"Six of Crows",
+      author:"Leigh Bardugo",
+      titles:"Six of Crows, Crooked Kingdom",
+      tgs:"fantasy romance ya"
+    },
+    {
+      name:"Sheets",
+      author:"Brenna Thumler",
+      titles:"Sheets, Delicates",
+      tgs:"graphic historical paranormal"
+    },
+    {
+      name:"Stepsister",
+      author:"Jennifer Donnelly",
+      titles:"Stepsister, Poisoned",
+      tgs:"fantasy ya"
+    },
+    {
+      name:"Story Thieves",
+      author:"James Riley",
+      titles:"Story Thieves, The Stolen Chapters, Secret Origins, Pick the Plot, Worlds Apart",
+      tgs:"kids worlds"
+    },
+    {
+      name:"The Beast Player",
+      author:"Nahoko Uehashi",
+      titles:"The Beast Player, The Beast Warrior",
+      tgs:"fantasy"
+    },
+    {
+      name:"The Boxcar Children",
+      author:"Gertrude Chandler Warner",
+      titles:"too many",
+      tgs:"mystery kids"
+    },
+    {
+      name:"The Chronicles of Narnia",
+      author:"C.S. Lewis",
+      titles:"The Magician's Nephew, The Lion, the Witch, and the Wardrobe, Prince Caspian, The Voyage of the Dawn Treader, The Silver Chair, The Horse and His Boy, The Last Battle",
+      tgs:"classic fantasy kids"
+    },
+    {
+      name:"The Grimm Legacy",
+      author:"Polly Shulman",
+      titles:"The Grimm Legacy, The Wells Bequest, The Poe Estate",
+      tgs:"fantasy paranormal sci-fi worlds ya"
+    },
+    {
+      name:"The Hitchhiker's Guide to the Galaxy",
+      author:"Douglas Adams",
+      titles:"The Hitchhiker's Guide to the Galaxy; The Restaurant at the End of the Universe; Life, the Universe and Everything; So Long, and Thanks for All the Fish; Mostly Harmless; And Another Thing...",
+      tgs:"sci-fi"
+    },
+    {
+      name:"The Hunger Games",
+      author:"Suzanne Collins",
+      titles:"The Hunger Games, Catching Fire, Mockingjay",
+      tgs:"classic dystopian"
+    },
+    {
+      name:"The Illuminae Files",
+      author:"Amie Kaufman, Jay Kristoff",
+      titles:"Illuminae, Gemina, Obsidian",
+      tgs:"ya sci-fi"
+    },
+    {
+      name:"The Inheritance Cycle",
+      author:"Christopher Paolini",
+      titles:"Eragon, Eldest, Brisingr, Inheritance",
+      tgs:"classic fantasy ya"
+    },
+    {
+      name:"The Land of Elyon",
+      author:"Patrick Carman",
+      titles:"The Dark Hills Divide, Beyond the Valley of Thorns, The Tenth City, Into the Mist, Stargazer",
+      tgs:"classic fantasy kids"
+    },
+    {
+      name:"The Land of Stories",
+      author:"Chris Colfer",
+      titles:"The Wishing Spell, The Enchantress Returns, A Grimm Warning, Beyond the Kingdoms, An Author's Odyssey, Worlds Collide",
+      tgs:"fantasy kids"
+    },
+    {
+      name:"The Last Kids on Earth",
+      author:"Max Brallier, Douglas Holgate",
+      titles:"The Last Kids on Earth, ... and the Zombie Parade, ... and the Nightmare King, ... and the Cosmic Beyond, ... and the Midnight Blade, ... and the Skeleton Road, ... and the Doomsay Race, ... and the Forbidden Fortress",
+      tgs:"kids graphic apocalypse"
+    },
+    {
+      name:"The Lunar Chronicles",
+      author:"Marissa Meyer",
+      titles:"Cinder, Scarlet, Cress, Winter",
+      tgs:"classic fantasy romance sci-fi ya"
+    },
+    {
+      name:"The Mysterious Benedict Society",
+      author:"Trenton Stewart",
+      titles:"The Mysterious Benedict Society, The Perilous Journey, The Prisoner's Dilemma",
+      tgs:"kids realistic"
+    },
+    {
+      name:"The Secrets of the Pied Piper",
+      author:"Matthew Cody",
+      titles:"The Peddler's Road, The Magician's Key, The Piper's Apprentice",
+      tgs:"fantasy kids"
+    },
+    {
+      name:"Pandava Quintet (Aru Shah)",
+      author:"Roshani Chokshi",
+      titles:"Aru Shah and the End of Time, ... and the Song of Death, ... and the Tree of Wishes, ... and the City of Gold, ... and the Nectar of Immortality",
+      tgs:"fantasy kids"
+    },
+    {
+      name:"Zita the Spacegirl, Mighty Jack",
+      author:"Ben Hatke",
+      titles:"Zita the Spacegirl, Legends of Zita the Spacegirl, The Return of Zita the Spacegirl, Mighty Jack, Mighty Jack and the Goblin King, Mighty Jack and Zita the Spacegirl",
+      tgs:"graphic kids sci-fi"
+    },
+  ],
+  books: [
+    {
+      name:"Birdwing",
+      author:"Rafe Martin",
+      tgs:"fantasy"
+    },
+    {
+      name:"Greetings from Witness Protection!",
+      author:"Jake Burt",
+      tgs:"realistic standalone"
+    },
+    {
+      name:"Heartless",
+      author:"Marissa Meyer",
+      tgs:"fantasy romance standalone ya"
+    },
+    {
+      name:"Opposite of Always",
+      author:"Justin A. Reynolds",
+      tgs:"ya realistic standalone romance"
+    },
+    {
+      name:"Pandemonium",
+      author:"Chris Wooding, Cassandra Diaz",
+      tgs:"fantasy graphic romance"
+    },
+    {
+      name:"Sparkers, Wildings",
+      author:"Eleanor Glewwe",
+      tgs:"fantasy kids"
+    },
+    {
+      name:"The Ghost Bride",
+      author:"Yangsze Choo",
+      tgs:"ya fantasy historical romance",
+    },
+    {
+      name:"The Language of Spells",
+      author:"Garret Weyr",
+      tgs:"kids fantasy historical"
+    },
+    {
+      name:"The Ones We're Meant to Find",
+      author:"Joan He",
+      tgs:"ya romance sci-fi standalone"
+    },
+    {
+      name:"The Star-Touched Queen, The Crown of Wishes",
+      author:"Roshani Chokshi",
+      tgs:"fantasy ya romance"
+    },
+    {
+      name:"The Way Things Work",
+      author:"David Macaulay",
+      tgs:"informational"
+    },
+    {
+     name:"Twenty Thousand Leagues Under the Seas",
+     author:"Jules Verne",
+     tgs:"classic sci-fi"
+    },
+    {
+      name:"Warcross",
+      author:"Marie Lu",
+      tgs:"ya sci-fi romance"
+    }
+  ],
+  authors: [
+    {
+      author:"Agatha Christie",
+      tgs:"mystery classic standalone series"
+    },
+    {
+      author:"Ally Carter",
+      tgs:"spy series kids ya romance"
+    },
+    {
+      author:"April Henry",
+      tgs:"ya mystery"
+    },
+    {
+      author:"Brandon Mull",
+      titles:"The Beyonders, The Five Kingdoms, Fablehaven, The Candy Shop War",
+      tgs:"fantasy realistic kids ya"
+    },
+    {
+      author:"Cressida Cowell",
+      titles:"How to Train Your Dragon, Wizards of Once",
+      tgs:"fantasy series kids"
+    },
+    {
+      author:"Gail Carson Levine",
+      titles:"Ella Enchanted, Fairest, The Two Princesses of Bamarre",
+      tgs:"fantasy classic standalone"
+    },
+    {
+      author:"Holly Black",
+      tgs:"ya fantasy series romance"
+    },
+    {
+      author:"James Ponti",
+      titles:"Dead City, Framed!, City Spies",
+      tgs:"mystery"
+    },
+    {
+      author:"Lauren Magaziner",
+      titles:"Wizardmatch, Pilfer Academy, The Only Thing Worse than Witches",
+      tgs:"kids fantasy mystery spy"
+    },
+    {
+      author:"Mary Downing Hahn",
+      tgs:"paranormal kids standalone"
+    },
+    {
+      author:"Rick Riordan",
+      titles:"Percy Jackson and the Olympians, The Kane Chronicles, The Heroes of Olympus, Magnus Chase and the Gods of Asgard, The Trials of Apollo, Daughter of the Deep",
+      tgs:"series fantasy kids"
+    },
+    {
+      author:"Sarah Beth Durst",
+      tgs:"fantasy standalone kids"
+    },
+    {
+      author:"Sarah Dessen",
+      tgs:"ya realistic romance standalone"
+    },
+    {
+      author:"Stuart Gibbs",
+      titles:"FunJungle, Moon Base Alpha, Spy School",
+      tgs:"spy mystery series"
+    },
+    {
+      author:"Vera Brosgol",
+      titles:"Anya's Ghost, Be Prepared, Leave Me Alone!</i> (yeah, it's a picture book, but it's awesome!)<i>",
+      tgs:"graphic standalone realistic paranormal"
+    },
+  ],
+};
+var types = {
+  kids: "🟢",
+  ya: "🟠",
+  graphic: "🔵",
+};
